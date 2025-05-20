c94795d11248be4475c6fcf5bfad384a72ad6874
YAIRA CRISTINA QUINTERO SAMPAYO <yairaquintero299611@correo.itm.edu.co>
2025-05-20 01:44:28 -0500

    Intentocontroladorcountry

---
+++ ShoppingAPI/Controllers/CountryController.cs
@@ -0,0 +1,20 @@
+﻿/*using Microsoft.AspNetCore.Mvc;
+using ShoppingAPI.DAL.Entities;
+using ShoppingAPI.Domain.Interfaces;
+
+namespace ShoppingAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountryController:ControllerBase
+    {
+
+            private readonly ICountryService _countryService;
+
+            public CountryController(ICountryService countryService)
+            {
+                _countryService = countryService;
+            }*/
+
+       
--- ShoppingAPI/Program.cs
+++ ShoppingAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ShoppingAPI.DAL;
+using ShoppingAPI.Domain.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
